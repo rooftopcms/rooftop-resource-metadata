d0d5264936c52a5940f5577eeb8321124967812d
Paul Hendrick <paul@errorstudio.co.uk>
2017-06-30 13:51:12 +0100

    scaffolded test suite

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Rooftop_Resource_Metadata
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
